1d64cbd40aca7ecb8e644d2a732155ea35e18fb7
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-05-28 03:11:41 +0530

    Added new models

--- text/models.py
+++ text/models.py
@@ -11,8 +11,24 @@ import numpy as np
 import tensorflow as tf
 
 
+class MLP(tf.keras.Model):
+    def __init__(self, width, num_class, out_act):
+        print("Initiated Multi-Layer Perceptron Model.")
+        super(MLP, self).__init__()
+        self.layer1 = tf.keras.layers.Dense(units=width)
+        self.layer2 = tf.keras.layers.Dense(units=width)
+        self.output_layer = tf.keras.layers.Dense(units=num_class, activation=out_act)
+
+    def call(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.output_layer(x)
+        return x
+
+
 class SimpleRNN(tf.keras.Model):
     def __init__(self, width, num_class, out_act):
+        print("Initiated SimpleRNN Model.")
         super(SimpleRNN, self).__init__()
         self.layer1 = tf.keras.layers.SimpleRNN(units=width, return_sequences=True)
         self.layer2 = tf.keras.layers.SimpleRNN(units=width)
@@ -27,6 +43,7 @@ class SimpleRNN(tf.keras.Model):
 
 class SimpleLSTM(tf.keras.Model):
     def __init__(self, width, num_class, out_act):
+        print("Initiated SimpleLSTM Model.")
         super(SimpleLSTM, self).__init__()
         self.lstm_layer1 = tf.keras.layers.LSTM(units=width, return_sequences=True)
         self.lstm_layer2 = tf.keras.layers.LSTM(units=width)
@@ -41,6 +58,7 @@ class SimpleLSTM(tf.keras.Model):
 
 class SimpleGRU(tf.keras.Model):
     def __init__(self, width, num_class, out_act):
+        print("Initiated SimpleGRU Model.")
         super(SimpleGRU, self).__init__()
         self.layer1 = tf.keras.layers.GRU(units=width, return_sequences=True)
         self.layer2 = tf.keras.layers.GRU(units=width)
@@ -51,4 +69,3 @@ class SimpleGRU(tf.keras.Model):
         x = self.layer2(x)
         x = self.output_layer(x)
         return x
-
