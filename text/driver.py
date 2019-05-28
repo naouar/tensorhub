6171649d4e75d06a463760120725b73810224135
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-05-29 01:49:24 +0530

    Updated for loading pre-trained word embeddings

--- text/driver.py
+++ text/driver.py
@@ -15,17 +15,14 @@ class SequenceClassification():
     def __init__(self):
         pass
 
-    def get_simple_rnn(self, max_features, width, num_class, out_act):
-        return SimpleRNN(max_features, width, num_class, out_act)
+    def get_simple_rnn(self, max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding=False, embedding_matrix=None):
+        return SimpleRNN(max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding, embedding_matrix)
 
-    def get_simple_lstm(self, max_features, width, num_class, out_act):
-        return SimpleLSTM(max_features, width, num_class, out_act)
+    def get_simple_lstm(self, max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding=False, embedding_matrix=None):
+        return SimpleLSTM(max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding, embedding_matrix)
         
-    def get_simple_gru(self, max_features, width, num_class, out_act):
-        return SimpleGRU(max_features, width, num_clas, out_act)
-    
-    def get_mlp(self, width, num_class, out_act):
-        return MLP(width, num_class, out_act)
+    def get_simple_gru(self, max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding=False, embedding_matrix=None):
+        return SimpleGRU(max_length, vocab_size, num_classes, num_nodes, activation, learn_embedding, embedding_matrix)
 
 
 class NamedEntityRecognition():
