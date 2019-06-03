a41dc65654f65d899840524cb3c03df4c934e7b3
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-04 02:07:36 +0530

    Updated modules

--- sequence-classification/run_classifier.py
+++ sequence-classification/run_classifier.py
@@ -11,9 +11,10 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.python.keras.api import keras
+from sklearn.model_selection import train_test_split
 
 from driver import SequenceClassification
-from utils import data_loader, load_embedding, create_voabulary
+from utils import data_loader, load_embedding, create_tokenizer
 
 
 
@@ -29,53 +30,28 @@ print("Data Shape:", df.shape) # Data Shape: (200853, 6)
 print("Columns:", df.columns) # Columns: Index(['authors', 'category', 'date', 'headline', 'link', 'short_description'], dtype='object')
 
 # Select appropriate columns and create a numpy array with proper shape
-x_train = np.asarray(df.headline).reshape((df.shape[0], 1))
-y_train = np.asarray(df.category).reshape((df.shape[0], 1))
+x = list(df.headline)
+y = list(df.category)
 
 # Split data into train and test
-test_ratio = 0.3
-test_indexes = np.random.randint(low=0, high=df.shape[0], size=int(df.shape[0]*test_ratio))
-train_indexes = np.random.randint(low=0, high=df.shape[0], size=int(df.shape[0]*(1-test_ratio)))
-x_train, x_test = x_train[train_indexes,: ], x_train[test_indexes,: ]
-y_train, y_test = y_train[train_indexes,: ], y_train[test_indexes,: ]
+x_train, x_test, y_train, y_test = train_test_split(x, y) # Default test size of 25%
 
-# Convert numpy array into list
-x_train = x_train.tolist()
-y_train = y_train.tolist()
-x_test = x_test.tolist()
-y_test = y_test.tolist()
-
-print("Train Shape:", len(x_train)) # Train Shape: (140597, 1)
-print("Test Shape:", len(x_test)) # Test Shape: (60255, 1)
+print("Train samples:", len(x_train)) # Train size: 150639
+print("Test samples:", len(x_test)) # Test size: 50214
 
 # A dictionary mapping words to an integer index
 # Collect all textual data: Merge data from headlines and short description
-text_data = list()
-text_data.extend(list(df.headline))
-text_data.extend(list(df.short_description))
+corpus = list()
+corpus.extend(x_train)
+corpus.extend(x_test)
 
-# Create a custom tokenizer
-tokenizer = keras.preprocessing.text.Tokenizer()
+# Compute the maximum number of words in a sequence
+max_num_words = len(max(corpus).split())
 
-# Fit tokenizer on the corpus
-tokenizer.fit_on_texts(text_data)
+# Train a custom tokenier on the corpus and generate tokenizer instance and word vocabulary
+tokenizer, word_index = create_tokenizer(corpus)
 
-word_index = tokenizer.word_index # Get vocabulary
-
-print("Number of unique tokens in vocabulary are:", len(word_index) + 1) # 116618
-
-# Restructure the word_index and save other members
-# Shift everything by 3
-# Original mapping starts from 1
-word_index = {k:(v+3) for k, v in word_index.items()}
-word_index["<PAD>"] = 0
-word_index["<START>"] = 1
-word_index["<UNK>"] = 2
-word_index["<END>"] = 3
-
-print("Number of unique tokens in vocabulary are:", len(word_index) + 1) # 116618 + 4
-
-# Tokenize data
+# Tokenize data using the created tokenizer
 x_train = tokenizer.texts_to_sequences(x_train)
 x_test = tokenizer.texts_to_sequences(x_test)
 
@@ -85,7 +61,7 @@ x_train = keras.preprocessing.sequence.pad_sequences(
     value=word_index["<PAD>"],
     padding="post",
     truncating="post",
-    maxlen=128
+    maxlen=max_num_words
 )
 
 x_test = keras.preprocessing.sequence.pad_sequences(
@@ -93,7 +69,7 @@ x_test = keras.preprocessing.sequence.pad_sequences(
     value=word_index["<PAD>"],
     padding="post",
     truncating="post",
-    maxlen=128
+    maxlen=max_num_words
 )
 
 # Convert data type to float from int
@@ -104,17 +80,16 @@ x_test = x_test.astype(np.float32)
 classes = np.unique(y_train)
 # Generate a unique index for each class
 index = range(len(classes))
-# Create class to idnex mapping
+# Create class to index mapping
 class_index = dict(zip(classes, index))
 
 def numerify(labels):
     numerified_labels = list()
     for label in labels:
-        label = label[0] # All labels are store in a list
         numerified_labels.append(class_index[label])
     return numerified_labels
 
-# Integer encode labels
+# Now encode labels using the created mapping
 y_train = numerify(y_train)
 y_test = numerify(y_test)
 
@@ -130,7 +105,7 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 sequence_classifier = SequenceClassification()
 my_model = sequence_classifier.get_simple_lstm(
     vocab_size=len(word_index) + 1,
-    max_length=128, # Default: 512
+    max_length=max_num_words, # Default: 512
     num_nodes=256, # Default: 512
     num_classes=len(classes),
     learn_embedding=True, # Default
@@ -182,24 +157,16 @@ epochs = 2
 template = "Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}"
 
 # Run
+print("{:#^50s}".format("Train and Validate"))
 for epoch in range(epochs):
-    print("Training:", end=" ")
+    print("Epoch {}/{}".format(epoch, epochs))
     # Run model on batches
     for text, label in train_ds:
-        print(".", end="")
         train_step(text, label)
-    print()
-    print("Tetsing:", end=" ")
     # Test model on batches    
     for t_text, t_label in test_ds:
-        print(".", end="")
         test_step(t_text, t_label)
-    print()
     # Prompt user using defined template
     print(template.format(epoch+1, \
         train_loss.result(), train_accuracy.result()*100, \
         test_loss.result(), test_accuracy.result()*100))
-
-
-if __name__ == "__main__":
-    text_classifier()
