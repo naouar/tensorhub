22669af4b4860e9e1305a6b6bdb31889aed5c69d
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-07 16:52:54 +0530

    Learn embedding bug fixed

--- sequence-classification/models.py
+++ sequence-classification/models.py
@@ -76,10 +76,10 @@ class TextCNN(keras.Model):
     def __init__(self, vocab_size, num_classes, max_length, num_nodes, num_filter, kernal_size, stride, dropout_rate, activation, output_activation, learn_embedding, embedding_matrix):
         print("Initiated TextCNN Model.")
         super(TextCNN, self).__init__()
-        if learn_embedding == False:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=num_nodes, weights=[embedding_matrix], trainable=False, input_length=max_length)
-        else:
+        if learn_embedding == True:
             self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=num_nodes, input_length=max_length)
+        else:
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=num_nodes, weights=[embedding_matrix], trainable=False, input_length=max_length)
         self.dropout_layer = keras.layers.Dropout(rate=dropout_rate)
         self.con_layer = keras.layers.Conv1D(filters=num_filter, kernel_size=(kernal_size), padding="valid", activation=activation, strides=stride)
         self.pool_layer = tf.keras.layers.GlobalMaxPooling1D()
