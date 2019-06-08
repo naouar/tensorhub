fec60a070acc3b33fb7e8914205bba4001892004
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-08 13:05:22 +0530

    Updated model embeddings

--- text-classification/models.py
+++ text-classification/models.py
@@ -14,15 +14,14 @@ from tensorflow.python.keras.api import keras
 
 class SimpleRNN(keras.Model):
     def __init__(self, vocab_size, num_classes, max_length=512, activation="relu", output_activation="softmax", learn_embedding=True, embedding_matrix=None):
-        print("Initiated SimpleRNN Model.")
         super(SimpleRNN, self).__init__()
-        if learn_embedding == False:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
-        else:
+        if learn_embedding == True:
             self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, input_length=max_length, mask_zero=True)
-        self.rnn_layer = keras.layers.SimpleRNN(units=512, return_sequences=True, activation=activation)
+        else:
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
+        self.rnn_layer = keras.layers.SimpleRNN(units=512, activation=activation)
         self.dense_layer1 = keras.layers.Dense(units=1024, activation=activation)
-        self.dense_layer1 = keras.layers.Dense(units=1024, activation=activation)
+        self.dense_layer2 = keras.layers.Dense(units=1024, activation=activation)
         self.output_layer = keras.layers.Dense(units=num_classes, activation=output_activation)
     
     def call(self, x):
@@ -36,13 +35,12 @@ class SimpleRNN(keras.Model):
 
 class SimpleLSTM(keras.Model):
     def __init__(self, vocab_size, num_classes, max_length=512, activation="relu", output_activation="softmax", learn_embedding=True, embedding_matrix=None):
-        print("Initiated SimpleLSTM Model.")
         super(SimpleLSTM, self).__init__()
-        if learn_embedding == False:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
-        else:
+        if learn_embedding == True:
             self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, input_length=max_length, mask_zero=True)
-        self.lstm_layer = keras.layers.LSTM(units=512, return_sequences=True, activation=activation)
+        else:
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
+        self.lstm_layer = keras.layers.LSTM(units=512, activation=activation)
         self.dense_layer1 = keras.layers.Dense(units=1024, activation=activation)
         self.dense_layer2 = keras.layers.Dense(units=1024, activation=activation)
         self.output_layer = keras.layers.Dense(units=num_classes, activation=output_activation)
@@ -58,13 +56,12 @@ class SimpleLSTM(keras.Model):
 
 class SimpleGRU(keras.Model):
     def __init__(self, vocab_size, num_classes, max_length=512, activation="relu", output_activation="softmax", learn_embedding=True, embedding_matrix=None):
-        print("Initiated SimpleGRU Model.")
         super(SimpleGRU, self).__init__()
-        if learn_embedding == False:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
-        else:
+        if learn_embedding == True:
             self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, input_length=max_length, mask_zero=True)
-        self.gru_layer = keras.layers.GRU(units=512, return_sequences=True, activation=activation)
+        else:
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=512, weights=[embedding_matrix], trainable=False, input_length=max_length)
+        self.gru_layer = keras.layers.GRU(units=512, activation=activation)
         self.dense_layer1 = keras.layers.Dense(units=1024, activation=activation)
         self.dense_layer2 = keras.layers.Dense(units=1024, activation=activation)
         self.output_layer = keras.layers.Dense(units=num_classes, activation=output_activation)
@@ -79,26 +76,35 @@ class SimpleGRU(keras.Model):
 
 
 class TextCNN(keras.Model):
-    def __init__(self, vocab_size, num_classes, max_length, num_nodes, num_filter, kernal_size, stride, dropout_rate, activation, output_activation, learn_embedding, embedding_matrix):
-        print("Initiated TextCNN Model.")
+    def __init__(self, vocab_size, num_classes, max_length=512, activation="relu", output_activation="softmax", learn_embedding=True, embedding_matrix=None):
         super(TextCNN, self).__init__()
-        if learn_embedding == False:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=num_nodes, weights=[embedding_matrix], trainable=False, input_length=max_length)
+        if learn_embedding == True:
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=100, input_length=max_length)
         else:
-            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=num_nodes, input_length=max_length, mask_zero=True)
-        self.spatial_dropout_layer = keras.layers.SpatialDropout1D(rate=0.2)
-        self.cnn_layer = keras.layers.Conv1D(filters=num_filter, kernel_size=(kernal_size), padding="valid", activation=activation, strides=stride)
-        self.pool_layer = tf.keras.layers.GlobalMaxPooling1D()
-        self.dense_layer1 = keras.layers.Dense(units=1024, activation=activation)
-        self.dense_layer2 = keras.layers.Dense(units=1024, activation=activation)
+            self.embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=100, weights=[embedding_matrix], trainable=False, input_length=max_length)
+        self.dropout_layer = keras.layers.Dropout(rate=0.2)
+        self.reshape_layer = keras.layers.Reshape((100, max_length))
+        self.conv_layer1 = keras.layers.Conv1D(filters=64, kernel_size=(5), activation=activation)
+        self.pool_layer1 = keras.layers.GlobalMaxPool1D()
+        self.conv_layer2 = keras.layers.Conv1D(filters=128, kernel_size=(5), activation=activation)
+        self.pool_layer2 = keras.layers.GlobalMaxPool1D()
+        self.conv_layer3 = keras.layers.Conv1D(filters=256, kernel_size=(5), activation=activation)
+        self.pool_layer3 = keras.layers.GlobalMaxPool1D()
+        self.flat_layer = keras.layers.Flatten()
+        self.dense_layer = keras.layers.Dense(units=1024, activation=activation)
         self.output_layer = keras.layers.Dense(units=num_classes, activation=output_activation)
     
     def call(self, x):
         x = self.embedding_layer(x)
-        x = self.spatial_dropout_layer(x)
-        x = self.cnn_layer(x)
-        x = self.pool_layer(x)
-        x = self.dense_layer1(x)
-        x = self.dense_layer2(x)
+        x = self.dropout_layer(x)
+        x = self.reshape_layer(x)
+        x = self.conv_layer1(x)
+        x = self.pool_layer1(x)
+        x = self.conv_layer2(x)
+        x = self.pool_layer2(x)
+        x = self.conv_layer3(x)
+        x = self.pool_layer3(x)
+        x = self.flat_layer(x)
+        x = self.dense_layer(x)
         x = self.output_layer(x)
         return x
