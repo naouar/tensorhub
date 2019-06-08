529c7b6b743f2f8e939e002f3e8ce08c7158e833
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-08 18:38:52 +0530

    Added class structure

--- text-classification/utils.py
+++ text-classification/utils.py
@@ -62,54 +62,73 @@ class data_loader:
         return df
 
 
-def create_embeddings(text_data, type_embedding):
-    """Method to create a tokenizer and generate word vocabulary.
+class Embeddings():
+    """Class implementing plaground for embeddings."""
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_embeddings(text_data, type_embedding):
+        """Method to create a tokenizer and generate word vocabulary.
+        
+        Arguments:
+            text_data {List} -- A list of strings from which word-index or char-index mapping is created. \
+                Generally it is the entire corpus of text available.
+        """
+        # Create a custom tokenizer
+        if type_embedding.lower() == "word":
+            # Word embeddings
+            tokenizer = keras.preprocessing.text.Tokenizer(char_level=False, oov_token="<UNK>")
+        else:
+            # Character embeddings
+            tokenizer = keras.preprocessing.text.Tokenizer(char_level=True, oov_token="<UNK>")
+        # Fit tokenizer on the corpus
+        tokenizer.fit_on_texts(text_data)
+
+        word_index = tokenizer.word_index # Get vocabulary
+        print("Number of unique tokens in vocabulary are:", len(word_index) + 1) # 116618
+
+        # Restructure the word_index and save other members
+        # Shift everything by 3
+        # Original mapping starts from 1
+        return tokenizer, word_index
+
+    @staticmethod
+    def load_embedding(filepath, dim, word_index):
+        """Create an embedding matrix from the given pre-trained vector.
+        
+        Arguments:
+            filepath {Strign} -- Location to the pre-trained embedding.
+            dim {Int} -- An integer denoting the embedding dimension of the pre-trained embedding.
+            word_index {Dict} -- A dictionary containing word-index mapping from the whole corpus of train and test.
+        
+        Returns:
+            NumpyArray -- A numpy matrix containing embeddings for words in the word-index mapping.
+        """
+        embedding_index = dict()
+        with open(filepath, mode="r") as fp:
+            for line in fp:
+                values = line.split()
+                word = values[0:-dim]
+                coefs = values[-dim:]
+                embedding_index[word[0]] = coefs
+        # Create a weight matrix for words in training docs
+        embedding_matrix = np.zeros((len(word_index), dim))
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+        return embedding_matrix
+
+
+class Activations():
+    """Class implementing custom activation functions."""
+    def __init__(self):
+        pass
     
-    Arguments:
-        text_data {List} -- A list of strings from which word-index or char-index mapping is created. \
-            Generally it is the entire corpus of text available.
-    """
-    # Create a custom tokenizer
-    if type_embedding.lower() == "word":
-        # Word embeddings
-        tokenizer = keras.preprocessing.text.Tokenizer(char_level=False, oov_token="<UNK>")
-    else:
-        # Character embeddings
-        tokenizer = keras.preprocessing.text.Tokenizer(char_level=True, oov_token="<UNK>")
-    # Fit tokenizer on the corpus
-    tokenizer.fit_on_texts(text_data)
-
-    word_index = tokenizer.word_index # Get vocabulary
-    print("Number of unique tokens in vocabulary are:", len(word_index) + 1) # 116618
-
-    # Restructure the word_index and save other members
-    # Shift everything by 3
-    # Original mapping starts from 1
-    return tokenizer, word_index
-
-
-def load_embedding(filepath, dim, word_index):
-    """Create an embedding matrix from the given pre-trained vector.
+    def bert_gelu(x):
+        pass
     
-    Arguments:
-        filepath {Strign} -- Location to the pre-trained embedding.
-        dim {Int} -- An integer denoting the embedding dimension of the pre-trained embedding.
-        word_index {Dict} -- A dictionary containing word-index mapping from the whole corpus of train and test.
+    def gpt_gelu(x):
+        pass
     
-    Returns:
-        NumpyArray -- A numpy matrix containing embeddings for words in the word-index mapping.
-    """
-    embedding_index = dict()
-    with open(filepath, mode="r") as fp:
-        for line in fp:
-            values = line.split()
-            word = values[0:-dim]
-            coefs = values[-dim:]
-            embedding_index[word[0]] = coefs
-    # Create a weight matrix for words in training docs
-    embedding_matrix = np.zeros((len(word_index), dim))
-    for word, i in word_index.items():
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[i] = embedding_vector
-    return embedding_matrix
