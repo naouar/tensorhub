71d3bd8abb243ac8d365db86f8c942bc5ee480ee
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-11 00:37:53 +0530

    Initial commit

---
+++ run_nmt.py
@@ -0,0 +1,14 @@
+""" 
+@Author: Kumar Nityan Suman
+@Date: 2019-06-11 00:34:25
+"""
+
+# Load packages
+import os
+import sys
+import re
+import numpy as np
+from tensorflow.python.keras.api import keras
+
+
+"""Neural Machine Translation on German-English Dataset."""
