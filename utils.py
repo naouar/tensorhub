5e7dd27cbb5a20490490183cb8fd5feb53723f8b
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-15 22:05:29 +0530

    Updated modules

--- utils.py
+++ utils.py
@@ -5,106 +5,111 @@
 
 # Load packages
 import os
-import re
 import sys
 import numpy as np
 import pandas as pd
 from tensorflow.python.keras.api import keras
 
 
-"""Contains Utilities Methods for Models."""
+"""Contains Utilities Methods for Model Training and Validation."""
+
 
 class DataLoader:
-    """Template class for data loaders.
+    """Template class for loading data from multiple sources into a dataframe.
     
     Returns:
-        DataFrame -- Returns a df object.
+        DataFrame -- Returns a dataframe object.
     """
     def __init__(self):
+        """Class constructor.
+        """
         pass
 
     @staticmethod
     def load_json(filepath):
-        """Static method to load JSON data into DF objct.
+        """Load .json file into a dataframe object.
         
         Arguments:
-            filepath {String} -- JSON file path.
+            filepath {String} -- Path to source file.
         
         Returns:
-            DataFrame -- Returns a DF object.
+            DataFrame -- Returns a dataframe object.
         """
-        df = pd.read_json(filepath, orient="records", encoding="utf-8", lines=True)
-        return df
-    
+        return pd.read_json(filepath, orient="records", encoding="utf-8", lines=True)
+        
     @staticmethod
     def load_csv(filepath):
-        """Static method to load CSV data into DF objct.
+        """Load .csv file into a dataframe object.
         
         Arguments:
-            filepath {String} -- CSV file path.
+            filepath {String} -- Path to source file.
         
         Returns:
-            DataFrame -- Returns a DF object.
+            DataFrame -- Returns a dataframe object.
         """
-        df = pd.read_csv(filepath, encoding="utf-8")
-        return df
-    
+        return pd.read_csv(filepath, encoding="utf-8")
+        
     @staticmethod
     def load_tsv(filepath):
-        """Static method to load TSV data into DF objct.
+        """Load .tsv file into a dataframe object.
         
         Arguments:
-            filepath {String} -- TSV file path.
+            filepath {String} -- Path to source file.
         
         Returns:
-            DataFrame -- Returns a DF object.
+            DataFrame -- Returns a dataframe object.
         """
-        df = pd.read_csv(filepath, encoding="utf-8", sep="\t")
-        return df
+        return pd.read_csv(filepath, encoding="utf-8", sep="\t")
+        
 
-
-class Embeddings():
-    """Class implementing plaground for embeddings."""
+class Embeddings:
+    """Class implementing plaground for embeddings.
+    
+    Returns:
+        Matrix -- Embedding matrix.
+    """
     def __init__(self):
+        """Class Constructor.
+        """
         pass
 
     @staticmethod
-    def create_embeddings(text_data, type_embedding):
-        """Method to create a tokenizer and generate word vocabulary.
+    def create_vocabulary(list_text, type_embedding="word", num_words=10000):
+        """Create a sequence tokenizer and generate vocabulary.
         
         Arguments:
-            text_data {List} -- A list of strings from which word-index or char-index mapping is created. \
-                Generally it is the entire corpus of text available.
+            list_text {List} -- A list of strings from which word-index or char-index mapping is created.
+            num_words {Int} -- Maximum number of words to keep, based on word frequency. Only the most common num_words-1 words will be kept.
+        
+        Returns:
+            TokenizerObject -- Tokenizer object to fit sequences.
+            Dict -- Vocabulary dictionary.
         """
         # Create a custom tokenizer
         if type_embedding.lower() == "word":
             # Word embeddings
-            tokenizer = keras.preprocessing.text.Tokenizer(char_level=False, oov_token="<UNK>")
+            tokenizer = keras.preprocessing.text.Tokenizer(num_words=num_words, oov_token="<UNK>")
         else:
             # Character embeddings
             tokenizer = keras.preprocessing.text.Tokenizer(char_level=True, oov_token="<UNK>")
         # Fit tokenizer on the corpus
-        tokenizer.fit_on_texts(text_data)
-
-        word_index = tokenizer.word_index # Get vocabulary
-        print("Number of unique tokens in vocabulary are:", len(word_index) + 1) # 116618
-
-        # Restructure the word_index and save other members
-        # Shift everything by 3
-        # Original mapping starts from 1
-        return tokenizer, word_index
+        tokenizer.fit_on_texts(list_text)
+        # Access vocabulary
+        vocab = tokenizer.word_index
+        print("Number of unique tokens in vocabulary are:", len(vocab) + 1)
+        return tokenizer, vocab
 
     @staticmethod
-    def load_embedding(filepath, dim, word_index):
+    def load_embedding(word_index, filepath, dim=300):
         """Create an embedding matrix from the given pre-trained vector.
         
         Arguments:
-            filepath {Strign} -- Location to the pre-trained embedding.
-            dim {Int} -- An integer denoting the embedding dimension of the pre-trained embedding.
-            word_index {Dict} -- A dictionary containing word-index mapping from the whole corpus of train and test.
+            filepath {String} -- Path to load pre-trained embeddings (ex: glove).
+            dim {Int} -- Dimension of the embedding.
+            word_index {Dict} -- A dictionary containing word-index mapping from the whole corpus.
         
         Returns:
-            NumpyArray -- A numpy matrix containing embeddings for words in the word-index mapping.
+            Matrix -- A matrix containing embeddings for words in the word-index mapping.
         """
         embedding_index = dict()
         with open(filepath, mode="r") as fp:
@@ -119,17 +124,5 @@ class Embeddings():
             embedding_vector = embeddings_index.get(word)
             if embedding_vector is not None:
                 embedding_matrix[i] = embedding_vector
+        print("Number of unique tokens in vocabulary are:", len(embedding_matrix) + 1)
         return embedding_matrix
-
-
-class Activations():
-    """Class implementing custom activation functions."""
-    def __init__(self):
-        pass
-    
-    def bert_gelu(x):
-        pass
-    
-    def gpt_gelu(x):
-        pass
-    
