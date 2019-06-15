608df7e63506b15b9e85894ca864271bbc0eb023
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-15 22:57:14 +0530

    Initial commit

---
+++ nlp/transformer.py
@@ -0,0 +1,25 @@
+""" 
+@Author: Kumar Nityan Suman
+@Date: 2019-06-15 22:50:04
+"""
+
+
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras.api import keras
+
+
+"""Transfomer in different flavours."""
+
+class TransfomerEncoder:
+    """Classical transfomer architecture implementation."""
+    def __init__(self):
+        pass
+
+
+class EvolvedTransformerEncoder:
+    """Evolved transformer architecture implementation from AutoML."""
+    def __init__(self):
+        pass
