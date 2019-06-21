971644f3e5f4b68366e1b5cf972aae0026b54c14
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-21 18:56:58 +0530

    Restructured methods

--- text/blocks/utils.py
+++ text/blocks/utils.py
@@ -1,58 +1,16 @@
 """ 
 @Author: Kumar Nityan Suman
-@Date: 2019-05-28 22:18:30
+@Date: 2019-06-21 00:23:38
 """
 
 
 # Load packages
 import os
 import sys
-import pandas as pd
 
 
-class DataLoader:
-    """Template class for loading data from multiple sources into a dataframe.
-    
-    Returns:
-        DataFrame -- Returns a dataframe object.
-    """
+class Trainer:
+    """Template class for train and validation of model in development and production."""
+
     def __init__(self):
-        """Class constructor.
-        """
         pass
-
-    @staticmethod
-    def load_json(filepath):
-        """Load .json file into a dataframe object.
-        
-        Arguments:
-            filepath {String} -- Path to source file.
-        
-        Returns:
-            DataFrame -- Returns a dataframe object.
-        """
-        return pd.read_json(filepath, orient="records", encoding="utf-8", lines=True)
-        
-    @staticmethod
-    def load_csv(filepath):
-        """Load .csv file into a dataframe object.
-        
-        Arguments:
-            filepath {String} -- Path to source file.
-        
-        Returns:
-            DataFrame -- Returns a dataframe object.
-        """
-        return pd.read_csv(filepath, encoding="utf-8")
-        
-    @staticmethod
-    def load_tsv(filepath):
-        """Load .tsv file into a dataframe object.
-        
-        Arguments:
-            filepath {String} -- Path to source file.
-        
-        Returns:
-            DataFrame -- Returns a dataframe object.
-        """
-        return pd.read_csv(filepath, encoding="utf-8", sep="\t")
