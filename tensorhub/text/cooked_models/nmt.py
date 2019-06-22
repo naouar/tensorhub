de6a5e9a78781040a470685ebea964d4357dd505
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-23 00:47:19 +0530

    Restructuring for pypi

---
+++ tensorhub/text/cooked_models/nmt.py
@@ -0,0 +1,27 @@
+"""
+@Author: Kumar Nityan Suman
+@Date: 2019-06-22 01:55:26
+"""
+
+
+# Load packages
+from tensorflow import keras
+from blocks.encoder import Encoder
+from blocks.decoder import Decoder, AttentionDecoder
+
+
+class BasicNMT(keras.Model):
+    def __init__(self):
+        pass
+    
+    def call(self, x):
+        pass
+
+
+class AttentionNMT(keras.Model):
+    def __init__(self):
+        pass
+    
+    def call(self, x):
+        pass
+    
